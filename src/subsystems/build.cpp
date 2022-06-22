31ad68e6fe9a3cabfb9c83e79d31c435b68c8419
ayushsawnani <ayushsawnani@gmail.com>
2022-06-22 16:24:15 -0400

    added comments for reference

--- src/subsystems/build.cpp
+++ src/subsystems/build.cpp
@@ -4,10 +4,15 @@ static bool INTAKE_RUNNING = false;
 static bool FLYWHEEL_RUNNING = false; 
 void setBuildMotors() {
 
+    //In C++, 1 is true and 0 is false
+
     //sets the dispenser
     dispenser.set_value(master.get_digital(DIGITAL_R2));
 
     //sets the angler motor
+
+    //either will be 1 (up) or -1 (down) * 200 RPM, if both or neither are pressed it is just 0
+
     angler.move_velocity((master.get_digital(DIGITAL_UP) - master.get_digital(DIGITAL_DOWN) )* 200);
 
     //set roller motor
@@ -15,6 +20,9 @@ void setBuildMotors() {
 
     //sets expansion
     expander.move_velocity(master.get_digital(DIGITAL_B) * 200);
+
+
+    //1 is true and 0 is false
 
     //toggles intake motors
     if (master.get_digital(DIGITAL_A)) INTAKE_RUNNING = !INTAKE_RUNNING;
